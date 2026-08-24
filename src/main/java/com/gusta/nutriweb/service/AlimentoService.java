daec681306247d57ccaa5b67eba78db019f64191
Gustavo Maciel <gustavomsilva233@gmail.com>
2026-08-24 14:34:28 -0300

    feat: Adiciona o metodo buscarAlimentosPorId na classe AlimentoService

--- src/main/java/com/gusta/nutriweb/service/AlimentoService.java
+++ src/main/java/com/gusta/nutriweb/service/AlimentoService.java
@@ -3,12 +3,14 @@ package com.gusta.nutriweb.service;
 import com.gusta.nutriweb.model.AlimentoModel;
 import com.gusta.nutriweb.util.NumeroUtil;
 import com.opencsv.CSVReader;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static com.gusta.nutriweb.util.NumeroUtil.converterDouble;
 
@@ -18,24 +20,35 @@ public class AlimentoService {
     public List<AlimentoModel> alimentosTacoCSV(String csvPath) {
         List<AlimentoModel> alimentos = new ArrayList<>();
 
-        try (CSVReader reader = new CSVReader(new InputStreamReader(getClass().getResourceAsStream("/alimentos.csv")))) {
+        try (CSVReader reader = new CSVReader(new InputStreamReader(getClass().getResourceAsStream(csvPath)))) {
 
             List<String[]> linhas = reader.readAll();
             linhas.remove(0);
-            for (String[] partes : linhas){
+            for (String[] partes : linhas) {
                 long id = Long.parseLong(partes[0]);
                 String nome = partes[1];
                 double proteina = NumeroUtil.arredondarUmaCasa(converterDouble(partes[5]).orElse(0.0));
-                double carboidrato =NumeroUtil.arredondarUmaCasa(converterDouble(partes[8]).orElse(0.0));
+                double carboidrato = NumeroUtil.arredondarUmaCasa(converterDouble(partes[8]).orElse(0.0));
                 double lipidio = NumeroUtil.arredondarUmaCasa(converterDouble(partes[6]).orElse(0.0));
                 double caloria = NumeroUtil.arredondarUmaCasa((converterDouble(partes[3]).orElse(0.0)));
 
-                alimentos.add(new AlimentoModel(id, nome , proteina, carboidrato, lipidio, caloria));
+                alimentos.add(new AlimentoModel(id, nome, proteina, carboidrato, lipidio, caloria));
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-return alimentos;
+        return alimentos;
+    }
+
+    public ResponseEntity<AlimentoModel> buscarAlimentosPorId(Long id) {
+        return alimentosTacoCSV("/alimentos.csv")
+                .stream()
+                .filter(a -> a.getId().equals(id))
+                .findFirst()
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());gi
+
     }
 }
+
