9cfa00bca853df470936900fafe65a04f3b064cf
Gustavo Maciel <gustavomsilva233@gmail.com>
2026-08-24 12:14:28 -0300

    feat: Adiciona CSV da tabela Taco e implementa AlimentoService para leitura regra de negócio

---
+++ src/main/java/com/gusta/nutriweb/service/AlimentoService.java
@@ -0,0 +1,41 @@
+package com.gusta.nutriweb.service;
+
+import com.gusta.nutriweb.model.AlimentoModel;
+import com.gusta.nutriweb.util.NumeroUtil;
+import com.opencsv.CSVReader;
+import org.springframework.stereotype.Service;
+
+
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.gusta.nutriweb.util.NumeroUtil.converterDouble;
+
+@Service
+public class AlimentoService {
+
+    public List<AlimentoModel> alimentosTacoCSV(String csvPath) {
+        List<AlimentoModel> alimentos = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReader(new InputStreamReader(getClass().getResourceAsStream("/alimentos.csv")))) {
+
+            List<String[]> linhas = reader.readAll();
+            linhas.remove(0);
+            for (String[] partes : linhas){
+                long id = Long.parseLong(partes[0]);
+                String nome = partes[1];
+                double proteina = NumeroUtil.arredondarUmaCasa(converterDouble(partes[5]).orElse(0.0));
+                double carboidrato =NumeroUtil.arredondarUmaCasa(converterDouble(partes[8]).orElse(0.0));
+                double lipidio = NumeroUtil.arredondarUmaCasa(converterDouble(partes[6]).orElse(0.0));
+                double caloria = NumeroUtil.arredondarUmaCasa((converterDouble(partes[3]).orElse(0.0)));
+
+                alimentos.add(new AlimentoModel(id, nome , proteina, carboidrato, lipidio, caloria));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+return alimentos;
+    }
+}
